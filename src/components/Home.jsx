8b111351598db13e0c64a6e4127e0e6b2ec59929
M-Ahmad-Imran <mahmadimran1122@gmal.com>
2025-10-01 21:57:50 +0500

    .map array! hard code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "../App.css";
 import StudentCard from "./StudentCard";
-
+const STUDENT_DATA = [
+  { rollNo: 'L1F24PACS0009', name: 'Muhammad Ahmad', course: 'Information Tecchnology', department: 'Computer Science' },
+  { rollNo: 'L1F24PACS0010', name: 'Muhammad Ali', course: 'Enterpenureship', department: 'Computer Science' },
+  { rollNo: 'L1F24PACS0011', name: 'Muhammad Moiz', course: 'Assembly Language', department: 'Computer Science' },
+  { rollNo: 'L1F24PACS0012', name: 'Muhammad Waqas', course: 'Design Analysis Alogrithms', department: 'Computer Science' },
+  { rollNo: 'L1F24PACS0013', name: 'Muhammad Husain', course: 'Compiler Construction', department: 'Computer Science' },
+  { rollNo: 'L1F24PACS0014', name: 'Muhammad Waheed', course: 'Theory of Automata', department: 'Computer Science' },
+];
 const Home = () => {
   return (
     <section className="home">
@@ -10,36 +17,15 @@ const Home = () => {
         Our School Management System is a complete digital solution designed to simplify academic and administrative tasks. From tracking student records, managing attendance, and monitoring grades to streamlining communication between teachers, parents, and students — everything is handled in one place. With an intuitive interface and powerful tools, schools can save time, reduce paperwork, and focus more on what truly matters: quality education.
       </p>
       <div className="card-wrapper">
-        <StudentCard
-          name="Muhammad Ahmad"
-          rollNo="L1F24PACS0009"
-          course="Compiler Construction"
-          department="Computer Science - FOIT"
-        />
-        <StudentCard
-          name="Muhammad Ali"
-          rollNo="L1F24PACS00010"
-          course="Theory of Automata"
-          department="Computer Science - FOIT"
-        />
-        <StudentCard
-          name="Muhammad Hamza"
-          rollNo="L1F24PACS0011"
-          course="Information Security"
-          department="Computer Science - FOIT"
-        />
-        <StudentCard
-          name="Muhammad Moiz"
-          rollNo="L1F24PACS0012"
-          course="Design & Analysis Algorithms"
-          department="Computer Science - FOIT"
-        />
-        <StudentCard
-          name="Muhammad Waqas"
-          rollNo="L1F24PACS0013"
-          course="Assembly Language"
-          department="Computer Science - FOIT"
-        />
+        {STUDENT_DATA.map((student) => (
+          <StudentCard 
+          name={student.name} 
+          key={student.rollNo} 
+          rollNo={student.rollNo} 
+          course={student.course}
+          department={student.department}
+          />
+        ))}
       </div>
     </section>
   );

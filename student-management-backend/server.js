ac8a0eb73ddac2a96accc5c531b4e067efa90b6c
Muhammad Ahmad <161123416+M-Ahmad-Imran@users.noreply.github.com>
2025-11-01 18:57:47 +0500

    Update server.js

--- student-management-backend/server.js
+++ student-management-backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import studentRoutes from "./routes/studentRoutes.js";
 
-dotenv.config();
-connectDB();
+mongoose.connect("mongodb+srv://mahmadimran1122_db_user:85pw2A8GsbLKDFE2@cluster1.7a0elul.mongodb.net/?appName=Cluster1")
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://school-management-frontend"],
+    method: ["POST","GET","PUT",DELETE],
+    credentials: true  
+  }
+));
 app.use(express.json());
 
 // Routes
